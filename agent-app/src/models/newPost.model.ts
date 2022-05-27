89d7e72ed630022f52e5b8a14276edc71fe8d3ce
Ognjen Srdic <srdic.ra46.2018@uns.ac.rs>
2022-05-27 15:22:19 +0200

    create post v0.5

---
+++ agent-app/src/models/newPost.model.ts
@@ -0,0 +1,4 @@
+export class NewPost {
+    image: String = '';
+    comment: String = '';
+}
--- agent-app/src/services/post.service.ts
+++ agent-app/src/services/post.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { UserService } from "./user.service";
 import { AllUserPostsResponse } from "src/models/post.model";
 import { PostComment } from "src/models/postComment.model";
+import { NewPost } from "src/models/newPost.model";
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +22,9 @@ export class PostService {
 
   commentPost(postId: String, comment: PostComment) {
     return this.http.put(this.apiUrl + '/' + postId + '/comment', JSON.stringify(comment));
+  }
+
+  posting(newPost: NewPost) {
+    return this.http.post<any>(this.apiUrl, JSON.stringify(newPost));
   }
 }
