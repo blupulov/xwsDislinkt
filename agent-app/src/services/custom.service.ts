d1d2d340be2e58b25fcd61b871d7cff4dccb2ca5
Bojan <lupulov.ra77.2018@uns.ac.rs>
2022-05-29 00:55:06 +0200

    showing following users and followers

---
+++ agent-app/src/services/custom.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Router } from "@angular/router";
+import { User } from "src/models/user.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CustomService {
+
+  constructor (private http: HttpClient, private router: Router) { }
+
+  private apiUrl = 'http://localhost:8001/custom';
+
+  getUserFollowers(userId: string | null | undefined) {
+    return this.http.get<User[]>(this.apiUrl + '/user/' + userId + "/followers")
+  }
+
+  getFollowingUsers(userId: string | null | undefined) {
+    return this.http.get<User[]>(this.apiUrl + '/user/' + userId + "/following")
+  }
+
+  // samo id-jevi osoba koje ulogovani korisnik prati
+  setFollowing(usersIds: String[]) {
+    localStorage.setItem("following", JSON.stringify(usersIds))
+  }
+
+  getFollowing() {
+    return JSON.parse(localStorage.getItem("following") || "{}")
+  }
+
+  // nakon svakog uspesnog zapracivanja ubaci se u listu novi userId
+  pushFollowing(userId: string) {
+    let following = this.getFollowing()
+    following.push(userId)
+    this.setFollowing(following)
+  }
+  
+}
