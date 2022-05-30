ccdf229a7cf2c44cf138e4e6bac59e4f4c8f4a58
Bojan <lupulov.ra77.2018@uns.ac.rs>
2022-05-31 01:24:38 +0200

    add like method on front

--- agent-app/src/app/components/selected-user-posts/selected-user-posts.component.ts
+++ agent-app/src/app/components/selected-user-posts/selected-user-posts.component.ts
@@ -1,3 +1,4 @@
+import { VariableAst } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Post } from 'src/models/post.model';
 import { PostComment } from 'src/models/postComment.model';
@@ -18,16 +19,14 @@ export class SelectedUserPostsComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAllUserPosts()
-
   }
-
 
   getAllUserPosts() {
     this.postService.getAllUserPosts(this.userService.getSelectedUserId()).subscribe(
       res => {
         this.posts = res.posts
       },
-      err => {
+      () => {
         alert('loading posts problem')
       }
     )
@@ -46,18 +45,74 @@ export class SelectedUserPostsComponent implements OnInit {
   }
 
   onSubmit() {
-    //datum treba prilagotiti
     let comment = new PostComment();
     comment.commentOwnerId = new String(this.userService.getUserId());
     comment.commentContent = this.comment;
 
     this.postService.commentPost(this.selectedPostId, comment).subscribe(
-      res => {
+      () => {
         this.posts.find(post => post.id === this.selectedPostId)?.comments.push(comment);
         this.comment = '';
-      }, err => {
+      }, () => {
         alert('problem with sending comment')
       }
     )
   }
+
+  findPostById(postId: String): Post {
+    for(let post of this.posts)
+      if (post.id == postId)
+        return post
+    return new Post()
+  }
+
+  isUserLikePost(postId: String): boolean {
+    let post = this.findPostById(postId)
+
+    for (let fanId of post.fansIds)
+      if(fanId == this.userService.getUserId())
+        return true;
+
+    return false;
+  }
+
+  isUserDislikePost(postId: String): boolean {
+    let post = this.findPostById(postId)
+    
+    for (let haterId of post.hatersIds)
+      if(haterId == this.userService.getUserId())
+        return true;
+
+    return false;
+  }
+
+  like(postId: String){
+    this.postService.like(postId).subscribe(
+      res => {
+        this.addUserIdToPostFans(postId)
+      }, err => {
+        alert('problem with liking post')
+      }
+    )
+  }
+
+  addUserIdToPostFans(postId: String) {
+    for(let post in this.posts){
+      if(this.posts[post].id == postId) {
+        this.posts[post].fansIds.push(this.userService.getUserId() || "")
+        this.posts[post].postLikeNumber++
+        for(let haterId in this.posts[post].hatersIds){
+          if(this.posts[post].hatersIds[haterId] == this.userService.getUserId()){
+            this.posts[post].hatersIds.splice(parseInt(haterId), 1)
+            this.posts[post].postDislikeNumber--
+          }
+        }
+      }
+    }
+  }
+
+  dislike(postId: String) {
+
+  }
+  
 }
